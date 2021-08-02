05380311b388d9373c85c7784ba1adb089276325
Szymon Cader <szymon.sc.cader@gmail.com>
2021-08-02 22:48:30 +0200

    Add installation of zsh from source

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,5 +2,8 @@
 import pytest
 import requests
 
-def test_true(host):
-    pass
+def test_zsh_is_installed(host):
+    result = host.run("zsh --version")
+
+    assert not result.rc
+    assert "5.8" in result.stdout
